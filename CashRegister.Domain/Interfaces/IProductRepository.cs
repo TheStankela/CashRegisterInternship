f8ae97a9841b1341de18d8c72f7eea68e5bd90fc
Ognjen Stankovic <ognjenogistankovic2002@gmail.com>
2023-03-28 12:01:29 +0200

    Added product repository interface

---
+++ CashRegister.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using CashRegister.Domain.Models;
+
+namespace CashRegister.Infrastructure.Repositories
+{
+	public interface IProductRepository
+	{
+		public bool AddProduct(Product product);
+		public Task<Product> GetProductByIdAsync(int id);
+		public Task<List<Product>> GetAllProductsAsync();
+		public bool Save();
+		public bool DeleteProduct(Product product);
+		public bool UpdateProduct(Product product);
+		public bool ProductExists(int id);
+	}
+}
