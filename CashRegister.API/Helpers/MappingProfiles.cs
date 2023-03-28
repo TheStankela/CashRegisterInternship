b56bf34f2acc2ed6fdcbd527836e78c3ec972691
Ognjen Stankovic <ognjenogistankovic2002@gmail.com>
2023-03-28 12:00:23 +0200

    added automapper profiles

---
+++ CashRegister.API/Helpers/MappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CashRegister.API.Dto;
+using CashRegister.Domain.Models;
+
+namespace CashRegister.API.Helpers
+{
+	public class MappingProfiles : Profile
+	{
+        public MappingProfiles()
+        {
+            CreateMap<Product, ProductDto>();
+			CreateMap<Product, ProductDto>().ReverseMap();
+		}
+    }
+}
