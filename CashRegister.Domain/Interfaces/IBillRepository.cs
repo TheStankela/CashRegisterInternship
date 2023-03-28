36356c8250a290dd52aefa130e43a27ada680466
Ognjen Stankovic <ognjenogistankovic2002@gmail.com>
2023-03-28 14:20:25 +0200

    Added Bill repository interface

---
+++ CashRegister.Domain/Interfaces/IBillRepository.cs
@@ -0,0 +1,15 @@
+﻿using CashRegister.Domain.Models;
+
+namespace CashRegister.Domain.Interfaces
+{
+	public interface IBillRepository
+	{
+		public bool AddBill(Bill bill);
+		public Task<Bill> GetBillByBillNumberAsync(string billNumber);
+		public Task<List<Bill>> GetAllBillsAsync();
+		public bool Save();
+		public bool DeleteBill(Bill bill);
+		public bool UpdateBill(Bill bill);
+		public bool BillExists(string billNumber);
+	}
+}
