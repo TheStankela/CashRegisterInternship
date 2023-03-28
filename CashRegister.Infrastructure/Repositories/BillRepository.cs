39b87b068a0fb78be056f5f5445d50a1b54236df
Ognjen Stankovic <ognjenogistankovic2002@gmail.com>
2023-03-28 14:20:42 +0200

    Implemented all crud operations for bills

--- CashRegister.Infrastructure/Repositories/BillRepository.cs
+++ CashRegister.Infrastructure/Repositories/BillRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CashRegister.Domain.Interfaces;
+using CashRegister.Domain.Models;
+using CashRegister.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,50 @@ using System.Threading.Tasks;
 
 namespace CashRegister.Infrastructure.Repositories
 {
-    public class BillRepository
-    {
-    }
+	public class BillRepository : IBillRepository
+	{
+		private readonly CashRegisterDBContext _context;
+		public BillRepository(CashRegisterDBContext context)
+        {
+            _context = context;
+        }
+        public bool AddBill(Bill bill)
+		{
+			_context.Bills.Add(bill);
+			return Save();
+		}
+
+		public bool BillExists(string billNumber)
+		{
+			return _context.Bills.Any(x => x.BillNumber == billNumber);
+		}
+
+		public bool DeleteBill(Bill bill)
+		{
+			_context.Remove(bill);
+			return Save();
+		}
+
+		public Task<List<Bill>> GetAllBillsAsync()
+		{
+			return _context.Bills.ToListAsync();
+		}
+
+		public Task<Bill> GetBillByBillNumberAsync(string billNumber)
+		{
+			return _context.Bills.FirstOrDefaultAsync(x => x.BillNumber == billNumber);
+		}
+
+		public bool Save()
+		{
+			var saved = _context.SaveChanges();
+			return saved > 0 ? true : false;
+		}
+
+		public bool UpdateBill(Bill bill)
+		{
+			_context.Update(bill);
+			return Save();
+		}
+	}
 }
