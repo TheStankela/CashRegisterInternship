3caf503ca73538b80f748aa5e82bfcc9d0beeb4a
Ognjen Stankovic <ognjenogistankovic2002@gmail.com>
2023-04-05 19:51:45 +0200

    Created service for managing JWT authentication

---
+++ CashRegister.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using CashRegister.Domain.Models;
+
+namespace CashRegister.Application.Interfaces
+{
+    public interface IAuthService
+    {
+        User Authenticate(UserLogin userLogin);
+        string GenerateToken(User user);
+    }
+}
